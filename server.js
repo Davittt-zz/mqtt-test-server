b4c790449edb694cd5ba17b87c51477082f531cc
Davittt <ddonari@gmail.com>
2016-07-14 09:08:41 -0300

    Subcribe + save data

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
+
+var temperaturePost = require("./model/temperature");
 var express = require('express');
 var app = express();
 var path = require('path');
-
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://test.mosquitto.org');
-var mqtt    = require('mqtt');
 
-var deviceRoot = 'emdo/devices/' ;
-var temperaturePost = require("./model/temperature");
+var deviceRoot = 'emdo/devices/#' ;
+
 
 client.on('connect', function () {
   console.log('Estoy arriba: ' + deviceRoot);
-  client.subscribe('emdo/devices');
+  client.subscribe('emdo/devices/#');
   //client.subscribe(deviceRoot);
 });
 
 client.on('message', function (topic, message) {
   // message is Buffer 
-  console.log('Entra a client.on');
-  console.log(message.toString());
-
+  console.log('Entra a client.on, topic = ' + topic ) ;
+  console.log('Message lenght = ' + message ) ;
+  
+  message = JSON.parse(message);
+  console.log(JSON.stringify(message));
+  
   var post = new temperaturePost(
 	{
-	  uuid:  "12",
-	  name:  "312",
-	  kw1:   "123",
-	  kw2:   "3123",
+	  uuid:  message.uuid,
+	  kw1:   message.kw1,
+	  kw2:   message.kw2,
 	});
-	
-	console.log("Post Created");
-	
+
 	//save model to MongoDB
 	post.save(function (err) {
 	  if (err) {
@@ -41,23 +41,12 @@ client.on('message', function (topic, message) {
 	});
 });
 
-//app.use(express.static('/images'));
-
-//app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'html');
   app.set('view options', {layout: false});
-  //app.use(express.favicon());
-  //app.use(express.logger('dev'));
-  //app.use(express.bodyParser());
-  //app.use(express.methodOverride());
-  //app.use(app.router);
-  //app.use(require('stylus').middleware(__dirname + '/public'));
   app.use('/bower_components',  express.static(__dirname + '/bower_components'));
   app.use(express.static(path.join(__dirname, 'public')));
-//});
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
